072879db1f3acc5ab070ee2b01a0af710e12ac4a
Azizkhuja <asaydraxmonov15@gmail.com>
2024-03-14 21:29:27 +0500

    changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,27 @@
 import type {Metadata} from "next";
 import {Montserrat} from "next/font/google";
 import "./globals.css";
+import MainLayout from "@/components/Layout/MainLayout";
 
-const montserrat = Montserrat({ subsets: ["latin"] });
+const montserrat = Montserrat({subsets: ["latin"]});
 
 export const metadata: Metadata = {
-  title: "Salom Nurafshon",
-  description: "",
+    title: "Salom Nurafshon",
+    description: "",
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={montserrat.className}>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en">
+        <body className={montserrat.className}>
+        <MainLayout>
+            {children}
+        </MainLayout>
+        </body>
+        </html>
+    );
 }

8333cc8680e0c8f3b53fefe64a6b402482d0ff21
Azizkhuja <asaydraxmonov15@gmail.com>
2024-03-14 21:11:51 +0500

    Add fontawesome packages

--- components/Main/Main.tsx
+++ components/Main/Main.tsx
@@ -4,14 +4,14 @@
  */
 
 import Banners from "@/components/Main/Banners";
-// import Apartments from "@/components/Main/Apartments";
+import Apartments from "@/components/Main/Apartments";
 import Consultation from "@/components/Main/Consultation";
 
 export default function Main() {
     return (
         <div>
             <Banners/>
-            {/*<Apartments/>*/}
+            <Apartments/>
             <Consultation/>
         </div>
     )
